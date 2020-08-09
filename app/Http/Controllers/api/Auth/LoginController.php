1785c7c59c5a086163a4495df5a0b5588be4e29d
eddogustian <eddogustian78@gmail.com>
2020-08-09 09:21:03 +0700

    update

--- app/Http/Controllers/api/Auth/LoginController.php
+++ app/Http/Controllers/api/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class LoginController extends Controller
 {
